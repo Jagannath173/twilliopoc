347e7de203ff540af3a15ffe1e7116d5b7c55717
Jagannath173 <Jagannath.Patro@bilvantis.io>
2026-01-03 00:18:43 +0530

    fix: correct app name and script path; update production host and repository details

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
-        name: "AI Assitant",
-        script: "sc/server.js",
+        name: "ai-assitant",
+        script: "src/server.js",
         instances: "max",
         exec_mode: "cluster",
         env: {
@@ -14,9 +14,12 @@ module.exports = {
     deploy: {
         production: {
             user: "ubuntu",
-            host: "13.60.36.169",
+            host: "13.60.63.251",
             ref: "origin/main",
-            repo: "",
+            repo: "https://github.com/Jagannath173/Twillio-POC.git",
+            path: "/var/www/twilio-backend",
+            "post-deploy":
+                "npm install && pm2 reload ecosystem.config.js --env production",
 
         }
     }
